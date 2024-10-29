77cf6fa25021634486b40a7e25d6ca84268235f5
linomp <linomp95@hotmail.com>
2024-10-29 18:19:12 +0100

    add readme & first file structure

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # controltab
+
+- [ ] get guitar pro files from music-xml
+- [ ] get music-xml files from guitar pro
+
+- [ ] diff 2 xml files
+- [ ] recognize bar(s) from the diff
+- [ ] extract the bars to a nex xml file
+- [ ] transform new xmls to guitar pro
+- [ ] transfer guitar pro to js visualization lib
+
+nice to have
+- [ ] paint notes?t
---
+++ tuxcli/build_tuxguitar.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Install prerequisites
+sudo apt install -y wget unzip git build-essential default-jdk maven libwebkit2gtk-4.1-0 libfluidsynth-dev libjack-jackd2-dev libasound2-dev liblilv-dev libsuil-dev qtbase5-dev fonts-wqy-zenhei
+
+# Download and install SWT for Linux
+SWT_ZIP="swt-4.26-gtk-linux-$(uname -m).zip"
+SWT_DIR="swt-4.26-gtk-linux-$(uname -m)"
+wget "https://archive.eclipse.org/eclipse/downloads/drops4/R-4.26-202211231800/$SWT_ZIP"
+mkdir $SWT_DIR
+cd $SWT_DIR
+unzip "../$SWT_ZIP"
+mvn install:install-file -Dfile=swt.jar -DgroupId=org.eclipse.swt -DartifactId=org.eclipse.swt.gtk.linux -Dpackaging=jar -Dversion=4.26
+cd ..
+
+# Get the TuxGuitar sources
+git clone https://github.com/helge17/tuxguitar.git
+cd tuxguitar
+
+# Build and install
+cd desktop/build-scripts/tuxguitar-linux-swt-deb
+mvn -e clean verify -P native-modules
+sudo dpkg -i target/tuxguitar-*.deb
